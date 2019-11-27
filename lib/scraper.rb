93dcca017c79d0b909d4d25fd38917630cfd5bc1
Carla Holcomb <carla.programmer@gmail.com>
2019-11-27 18:05:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://www.filmsite.org/bestactor2.html"))
 ##puts doc.css(".headline-26OIBN").text
-tables = doc.css('div#mainBodyWrapper table')
+tables = doc.css('div#mainBodyWrapper table tr')
 #table = tables.css("table tr td")
 puts tables
 #table = tables.last
