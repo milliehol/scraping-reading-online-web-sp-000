8fdc9d6d1b9a712106d487ab7803db9ecdb0004a
Carla Holcomb <carla.programmer@gmail.com>
2019-11-27 18:04:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN").text
+##doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+
+doc = Nokogiri::HTML(open("https://www.filmsite.org/bestactor2.html"))
+##puts doc.css(".headline-26OIBN").text
+tables = doc.css('div#mainBodyWrapper')
+table = tables.css("table tr td")
+puts table
+#table = tables.last
+#table.search("tr").each do |tr|
+  #cells = []
+  #cells = tr.search("td")
+  #cells.each do |cell|
+    #text = []
+    #text = cell.text.strip
+    #puts text[17]
+  #end
+#end
